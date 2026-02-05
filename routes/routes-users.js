652eefc489aad6bf8260f77e1a10199a3a07aa2c
YaelAlarcon <124086036+YaelAlarcon@users.noreply.github.com>
2026-02-05 11:57:07 -0600

    loginController

--- routes/routes-users.js
+++ routes/routes-users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const media = require('../controllers/WEB/media.controller');
+const login = require('../controllers/WEB/login.controller');
 
 module.exports = () => {
     router.get('/test', media.getTest);
     router.post('/getMultimediaByGr', media.getMultimediaByGr);
+    router.post('/getAcces', login.getAcces);
     return router;
 }
